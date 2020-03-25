e2d5d97ff67111c26a48ce6685a7a135285079be
maaliskesakuu <tanja@prodi.fi>
2020-03-25 13:24:09 +0200

    added a Person app

---
+++ 2_task/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>  
+      <p>Hello, World!</p>
+      <Person />
+    </div>
+  );
+};
+
+export default App;
---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Person.css';
+
+const Person = () => {
+  return (
+    <div class="card">
+      <p>Hello, my name is Tanja and I am 44 years old.</p> 
+    </div>
+    );
+};
+
+export default Person;
