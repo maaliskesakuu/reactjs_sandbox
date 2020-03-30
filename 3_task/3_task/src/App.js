f3c9089525aa133b4e53490732bef6935704f021
maaliskesakuu <tanja@prodi.fi>
2020-03-30 10:58:06 +0300

    added state

--- 3_task/3_task/src/App.js
+++ 3_task/3_task/src/App.js
@@ -1,11 +1,15 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    likes: 0
+  };
+
   render() {
     return (
       <div>
-        <h1>Total likes: </h1>
+        <h1>Total likes: {this.state.likes}</h1>
       </div>
     );
   }  
