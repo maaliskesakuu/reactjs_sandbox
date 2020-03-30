70d35ac9e099413031ccab6c94b25cc804f728c1
maaliskesakuu <tanja@prodi.fi>
2020-03-30 11:23:51 +0300

    added buttons and functionality to them

--- 3_task/3_task/src/App.js
+++ 3_task/3_task/src/App.js
@@ -6,10 +6,28 @@ class App extends Component {
     likes: 0
   };
 
+  addHandler = () => {
+    console.log('test');
+    this.setState({ likes: this.state.likes + 1})
+  };
+
+  minusHandler = () => {
+    console.log('Test');
+    this.setState({ likes: this.state.likes - 1 })
+  };
+
+  resetHandler = () => {
+    console.log('testing');
+    this.setState({ likes: this.state.likes = 0 })
+  };
+
   render() {
     return (
       <div>
         <h1>Total likes: {this.state.likes}</h1>
+        <button onClick={this.addHandler}>Add a like</button>
+        <button onClick={this.minusHandler}>Remove a like</button>
+        <button onClick={this.resetHandler}>Reset</button>
       </div>
     );
   }  
