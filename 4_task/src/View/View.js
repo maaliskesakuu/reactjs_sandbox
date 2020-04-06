00b918d49260351d1a6db0d1e3dea9bee080549c
maaliskesakuu <tanja@prodi.fi>
2020-04-06 11:03:49 +0300

    corrected View app from form to div

--- 4_task/src/View/View.js
+++ 4_task/src/View/View.js
@@ -5,12 +5,12 @@ const View = props => {
   return (
     <div className="inputView">
       <h1>This is your input:</h1>
-      <form className="formView">
-        <label>First name:<p>{props.firstname}</p></label>
-        <label>Last name:<p>{props.lastname}</p></label>
-        <label>Phone number:<p>{props.phonenumber}</p></label>
-        <label>Message:<p>{props.message}</p></label>
-      </form>
+      <div className="formView">
+        <div>First name:<p>{props.firstname}</p></div>
+        <div>Last name:<p>{props.lastname}</p></div>
+        <div>Phone number:<p>{props.phonenumber}</p></div>
+        <div>Message:<p>{props.message}</p></div>
+      </div>
     </div>
   )
 }
