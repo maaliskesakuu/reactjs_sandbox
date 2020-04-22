65bba6e331cca9814eb03314d3c4e8da7d285e3d
maaliskesakuu <tanja@prodi.fi>
2020-04-22 11:29:01 +0300

    added lecture file

--- 7_task/src/index.js
+++ 7_task/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Lecture from './lecture';
 
 ReactDOM.render(
-  <App /> , document.getElementById('root')
+  <Lecture /> , document.getElementById('root')
 );
 
