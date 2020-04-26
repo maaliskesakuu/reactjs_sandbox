5b3097ae15ed6c16dbeec97094640740a8600653
maaliskesakuu <tanja@prodi.fi>
2020-04-26 19:52:09 +0300

    added fontawesome and made like-button work + plus added a new screenshot

--- 6_task/src/App.js
+++ 6_task/src/App.js
@@ -11,6 +11,7 @@ const cards = [
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dui tellus, malesuada nec maximus sed, dignissim et purus. Quisque commodo tellus et nunc varius, ut sodales dui mattis. Morbi et condimentum arcu. In eget molestie velit. Duis aliquet, ex id ornare mollis, felis turpis egestas arcu, at auctor enim lacus.',
 		img: 'https://source.unsplash.com/featured/?car',
+		likes: 0,
 	},
 	{
 		id: 2,
@@ -19,6 +20,7 @@ const cards = [
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet feugiat metus, eget suscipit erat. Quisque volutpat dapibus risus, et dapibus sem mattis at. Duis porttitor sodales ligula, eu lobortis nisl iaculis vel. Vivamus lobortis lorem quis ex congue.',
 		img: 'https://source.unsplash.com/featured/?truck',
+		likes: 0,
 	},
 	{
 		id: 3,
@@ -27,6 +29,7 @@ const cards = [
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo enim. Proin nibh tortor, interdum ut magna vel, porta tristique lacus. Praesent eget facilisis sapien. Praesent velit augue, interdum id massa non, cursus rhoncus.',
 		img: 'https://source.unsplash.com/featured/?scooter',
+		likes: 0,
 	},
 	{
 		id: 4,
@@ -35,46 +38,66 @@ const cards = [
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra, arcu et tincidunt vestibulum, tellus velit tincidunt leo, nec vestibulum dolor orci id tortor. Aenean venenatis magna id urna blandit, vel imperdiet elit accumsan. Fusce efficitur vel quam.',
 		img: 'https://source.unsplash.com/featured/?bicycle',
+		likes: 0,
 	},
 	{
 		id: 5,
 		title: 'Lorem',
 		author: 'Gary Joe',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elit mauris, commodo ac ornare non, iaculis id quam. Fusce a lectus vitae magna ultricies lobortis. Curabitur mi enim, tincidunt non fringilla non, commodo ullamcorper dui. Fusce mattis eget eros sit amet fringilla.',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elit mauris, commodo ac ornare non, iaculis id quam. Fusce a lectus vitae magna ultricies lobortis. Curabitur mi enim, tincidunt non fringilla non, commodo ullamcorper dui. Fusce mattis eget eros sit amet fringilla.',
 		img: 'https://source.unsplash.com/featured/?vespa',
+		likes: 0,
 	},
 ];
 
 class App extends Component {
 	state = {
 		cards: cards,
-		likes: 0
-  };
-  
-  removeHandler = removeId => {
-    const myOldCards = [...this.state.cards];
-    myOldCards.splice(removeId, 1);
-    this.setState({ cards: myOldCards });
-	}
-	
-	addLikes = id => {
+	};
+
+	removeHandler = (removeId) => {
+		const myOldCards = [...this.state.cards];
+		myOldCards.splice(removeId, 1);
+		this.setState({ cards: myOldCards });
+	};
+
+	addLikesHandler = (id) => {
+		//find out which card you clicked
+		const cardId = this.state.cards.findIndex((c) => {
+			return c.id === id;
+		});
+
+		//spread the card
+		const card = { ...this.state.cards[cardId] };
+
+		// add a new like to one card
+		card.likes += 1;
+
+		// take the full array and spread it
+		const cards = [...this.state.cards];
+
+		// add the updated card back to the array
+		cards[cardId] = card;
+
+		// update the array back to original with the updated likes
 		this.setState({
-			likes: this.state.likes + 1,
-		})
+			cards: cards,
+		});
 	};
 
 	render() {
 		const cardsList = this.state.cards.map((card, index) => {
 			return (
-        <Post
-          onclick={this.removeHandler.bind(this, index)}
+				<Post
+					onclick={this.removeHandler.bind(this, index)}
 					key={card.id}
 					title={card.title}
 					author={card.author}
 					description={card.description}
 					img={card.img}
-					likes={this.addLikes.bind(this, index)}
-					totalLikes={this.state.likes}
+					likes={card.likes}
+					clicklikes={this.addLikesHandler.bind(this, card.id)}
 				/>
 			);
 		});
--- 6_task/src/Post/Post.js
+++ 6_task/src/Post/Post.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Post.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Post = (props) => {
+const Post = props => {
 	return (
 		<div className="card">
 			<img src={props.img} alt={props.title} />
 			<h2>{props.title}</h2>
 			<p>{props.author}</p>
 			<p>{props.description}</p>
-      <button onClick={props.onclick}>Remove</button>
-      <button
-        className='like_container'
-        onClick={props.likes}
-      >Like</button>
-      <p id="total">Total likes: {props.totalLikes}</p>	
+			<button className="like_container" onClick={props.clicklikes}><FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+			</button>
+			<p>Likes: {props.likes}</p>
+			<button class="trash" onClick={props.onclick}>
+				<FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+			</button>
 		</div>
 	);
 };
