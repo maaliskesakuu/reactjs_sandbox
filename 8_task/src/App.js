ef4adeeb8c8e19e8e6ab01b32321cd873bbab391
maaliskesakuu <tanja@prodi.fi>
2020-04-27 12:27:58 +0300

    added lecture examples

---
+++ 8_task/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Simplerouting from './simplerouting';
+
+function App() {
+  return (
+    <div>
+<Simplerouting />
+    </div>
+  );
+}
+
+export default App;
---
+++ 8_task/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Simplerouting from './simplerouting';
+import Nestedrouting from './nestedrouting';
+
+
+ReactDOM.render(
+    <Nestedrouting />,
+  document.getElementById('root')
+);
