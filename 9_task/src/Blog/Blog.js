ec3df03695c2fb8b94f008ac837ae3bab40a46f8
maaliskesakuu <tanja@prodi.fi>
2020-05-04 23:27:21 +0300

    styled the cards

--- 9_task/src/Blog/Blog.js
+++ 9_task/src/Blog/Blog.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Photo from '../Photo';
 import Container from 'react-bootstrap/Container';
+import { Switch, Route, useRouteMatch, Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card, { CardFooter } from 'react-bootstrap/Card';
 
 const Blog = () => {
+	let match = useRouteMatch();
 	const [photos, setPhotos] = useState([]);
 
 	useEffect(() => {
@@ -20,10 +24,60 @@ const Blog = () => {
 	return (
 		<Container className="mt-5">
 			{photos.map((photo) => (
-				<Photo key={photo.id} title={photo.title} thumbnailUrl={photo.thumbnailUrl} />
+				<div key={photo.id}>
+					<Card
+						bg="dark"
+						text="white"
+						className="mt-5"
+						style={{ width: '15rem' }}
+					>
+						<Card.Body>
+							<Card.Title>{photo.title}</Card.Title>
+							<Card.Img
+								variant="bottom"
+								src={photo.thumbnailUrl}
+								alt={photo.title}
+							/>
+							<Button variant="light" className="mt-3">
+								<Link to={`${match.url}/${photo.id}`}>
+									<div>Read more</div>
+								</Link>
+							</Button>
+						</Card.Body>
+					</Card>
+				</div>
 			))}
 		</Container>
 	);
+	// const photoList = () => {
+	// 	return (
+	// 		<Container className="mt-5">
+	// 			{photos.map((photo) => (
+	// 				<div key={photo.id}>
+	// 					<Photo title={photo.title} thumbnailUrl={photo.thumbnailUrl} />
+	// 					<Button>
+	// 						<Link to={`${match.url}/${photo.id}`}></Link>Read more
+	// 					</Button>
+	// 				</div>
+	// 			))}
+	// 		</Container>
+	// 	);
+	// };
+
+	// return (
+	// 	<div>
+	// 		<Switch>
+	// 			<Route path="/blog/:photoId">
+	// 				<Photo />
+	// 			</Route>
+	// 			<Route path={match.path}>
+	// 				<Container>
+	// 					<div>{photoList}</div>
+	// 				</Container>
+	// 			</Route>
+	// 		</Switch>
+	// 	</div>
+	// );
 };
 
 export default Blog;
--- 9_task/src/Photo.js
+++ 9_task/src/Photo.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link, useParams } from 'react-router-dom';
 
 const Photo = ({ title, thumbnailUrl }) => {
-  return (
-    <div>
-      <h3>{title}</h3>
-      <img src={thumbnailUrl} alt="" />
-    </div>
-  );
-}
+	// let { photoId } = useParams();
+	// let photo = photos.find((p) => p.title === postId);
+
+	return (
+		<div>
+			<h3>{title}</h3>
+			<img src={thumbnailUrl} alt="" />
+			<Button variant="light">
+				<Link to="/blog">
+					<div>Back to blog page</div>
+				</Link>
+			</Button>
+		</div>
+	);
+};
 
 export default Photo;
